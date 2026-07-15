3ce48f1d11632f8ecd84248fbfcf9e8813c634a3
Daniel Nemeth <daniel.nemeth@cae.com>
2026-07-15 07:05:44 +0200

    FlyoutPlacement added for split button

--- projects/xaml-ui/src/lib/basic-input/SplitButton.ts
+++ projects/xaml-ui/src/lib/basic-input/SplitButton.ts
@@ -1,26 +1,32 @@
-import { Component, ContentChild, EventEmitter, Input, Output } from "@angular/core";
+import { Component, ContentChild, ElementRef, EventEmitter, Input, Output, ViewChild } from "@angular/core";
 import { ButtonComponent } from "./Button";
 import { DropDownButtonComponent } from "./DropDownButton";
 import { GridModule } from "../layout/Grid";
 import { CommonModule } from "@angular/common";
 import { FrameworkElementComponent } from "../FrameworkElement";
 import { FlyoutBaseComponent } from "../primitives/FlyoutBase";
+import { FlyoutPlacementMode } from "../Common";
 
 @Component({
   selector: 'SplitButton',
   template: `<Grid ColumnDefinitions="1fr auto">
     <Button CornerRadius="4px 0 0 4px" [IsEnabled]="IsEnabled" (Click)="onButtonClick()" [ngClass]="buttonClass"><ng-content/></Button>
-    <DropDownButton CornerRadius="0 4px 4px 0" Width="12px" (Click)="onDropDownClick()" [IsEnabled]="IsEnabled"/>
+    <DropDownButton #dropDown CornerRadius="0 4px 4px 0" Width="12px" (Click)="onDropDownClick()" [IsEnabled]="IsEnabled"/>
   </Grid>`,
   imports: [CommonModule, ButtonComponent, DropDownButtonComponent, GridModule]
 })
 export class SplitButtonComponent extends FrameworkElementComponent {
   @Input() IsEnabled: boolean = true;
 
+  @Input() Placement: FlyoutPlacementMode = 'BottomEdgeAlignedLeft';
+
   @Output() Click = new EventEmitter();
 
   @ContentChild('xaml-flyout')
   private _popup?: FlyoutBaseComponent;
+
+  @ViewChild('dropDown', { read: ElementRef })
+  private _dropDown?: ElementRef;
 
   protected get buttonClass() {
     return this._popup?.IsOpen ? 'flyout-open' : undefined;
@@ -35,6 +41,8 @@ export class SplitButtonComponent extends FrameworkElementComponent {
   protected onDropDownClick() {
     if (!this._popup || !this.IsEnabled) return;
 
+    this._popup.Placement = this.Placement;
+    if (this._dropDown) this._popup.Target = this._dropDown.nativeElement;
     this._popup.IsOpen = true;
   }
 }
